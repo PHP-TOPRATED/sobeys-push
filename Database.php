a7498b5ea69b21c959cf100f7f840788fd957a1a
Fei Yang <fei.yang@truinc.ca>
2015-09-24 12:16:17 +0800

    2015-09-24 (5)

--- Database.php
+++ Database.php
@@ -1,25 +1,17 @@
 <?php
 session_start();
 
-//define ("DB_HOST", "ec2-23-20-42-201.compute-1.amazonaws.com");  //env[DB_HOST] = ec2-23-20-42-201.compute-1.amazonaws.com
-//define ("DB_NAME", "sobeyspush");   //env[DB_NAME] = sobeyspush
-//define ("DB_USER", "deploy");  //env[DB_USER] = deploy
-//define ("DB_PASS", "JQvxZgFeL");   //env[DB_PASS] = JQvxZgFeL
-
-putenv("PHP_ENV=production");
-putenv("DB_HOST=ec2-23-20-42-201.compute-1.amazonaws.com");
-putenv("DB_NAME=sobeyspush");
-putenv("DB_USER=deploy");
-putenv("DB_PASS=JQvxZgFeL");
-
+define ("DB_HOST", "ec2-23-20-42-201.compute-1.amazonaws.com");  //env[DB_HOST] = ec2-23-20-42-201.compute-1.amazonaws.com
+define ("DB_NAME", "sobeyspush");   //env[DB_NAME] = sobeyspush
+define ("DB_USER", "deploy");  //env[DB_USER] = deploy
+define ("DB_PASS", "JQvxZgFeL");   //env[DB_PASS] = JQvxZgFeL
 
 class Database {
 
     var $_db = null;
 
     public function __construct() {
-//        $this -> _db = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        $this -> _db = new mysqli( getenv(DB_HOST), getenv(DB_USER), getenv(DB_PASS), getenv(DB_NAME));
+        $this -> _db = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 		if (!$this -> _db)
 			die('Could not connect: ' . mysql_error());
